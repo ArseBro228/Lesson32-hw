7434ccb91d042a7cf4edac28b8058b4a4f3cbd21
ArseBro228 <iaseshviliarsen@gmail.com>
2022-01-23 01:56:57 +0200

    Lesson32+hw

---
+++ main.js
@@ -0,0 +1,157 @@
+// let a = Number(prompt
+// ("сторона кадрата="))
+// alert
+// (`P=${a*4}`)
+
+// let x = Number(prompt
+// (`y=3*x**6-6*x**2-7, x=?`))
+// alert
+// (`y=3*${x}**6-6*${x}**2-7
+// y=${3*x**6-6*x**2-7}`)
+
+// let y = Number(prompt
+// (`a=?`))
+// let u = Number(prompt
+// (`b=?`))
+// alert((y*u)**0.5)
+
+// let a = Number(prompt
+// (`сторона квадрата =`))
+// alert(`площадь квадрата = ${a**2}`)
+
+// let a = Number(prompt
+// (`a=`))
+// let b = Number(prompt
+// (`b=`))
+// alert
+// (`S=${a*b}
+// P=${2*(a+b)}`)
+
+// let d = Number(prompt
+// (`L=пd, d=`))
+// alert
+// (`L=${3.14*d}`)
+
+// let a = Number(prompt
+// (`V=a**3
+// S=6*a**2
+// a=`))
+// alert
+// (`V=${a**3}
+// S=${6*(a**2)}`)
+
+// let a = Number(prompt
+// (`a=`))
+// let b = Number(prompt
+// (`b=`))
+// let c = Number(prompt
+// (`c=`))
+// alert
+// (`V=${a*b*c}
+// S=${2*((a*b)+(b*c)+(a*c))}`)
+
+// let R = Number(prompt
+// (`L=2*п*R
+// S=п*R**2
+// R=`))
+// alert
+// (`L=${2*3.14*R}
+// S=${3.14*(R**2)}`)
+
+// let y = Number(prompt
+// (`a=?`))
+// let u = Number(prompt
+// (`b=?`))
+// alert((y*u)/2)
+
+// let a = Number(prompt
+// (`a=`))
+// let b = Number(prompt
+// (`b=`))
+// alert
+// (`a**2+b**2=${a**2+b**2}
+// a**2-b**2=${a**2-b**2}
+// (a**2)*(b**2)=${(a**2)*(b**2)}
+// (a**2)/(b**2)=${(a**2)/(b**2)}`)
+
+// let a = Number(prompt
+// (`a=`))
+// let b = Number(prompt
+// (`b=`))
+// alert
+// (`a**2+b**2=${Math.abs(a)+Math.abs(b)}
+// a**2-b**2=${Math.abs(a)-Math.abs(b)}
+// (a**2)*(b**2)=${(Math.abs(a))*(Math.abs(b))}
+// (a**2)/(b**2)=${(Math.abs(a))/(Math.abs(b))}`)
+
+// let a = Number(prompt
+// (`a=`))
+// let b = Number(prompt
+// (`b=`))
+// let c = Number((a**2+b**2)**0.5)
+// alert
+// (`c=${c}
+// P=${a+b+c}`)
+
+// let R1 = Number(prompt
+// (`R1=`))
+// let R2 = Number(prompt
+// (`R2=`))
+// let S1 = 3.14*(R1**2)
+// let S2 = 3.14*(R2**2)
+// let S3 = S1-S2
+// alert
+// (`S1 = ${S1}
+// S2 = ${S2}
+// S3 = ${S3}`)
+
+// let L = Number(prompt
+// (`L=`))
+// let R = L/6.28
+// let S = 3.14*(R**2)
+// alert(`L=${L}
+// R=${R}
+// S=${S}`)
+
+// let S = Number(prompt
+// (`S=`))
+// let D = (S**2)*16/(3.14**2)
+// let L = 3.14*D
+// alert
+// (`D=${D}
+// L=${L}`)
+
+// let x = Number(prompt
+// (`y=4*((x-3)**6)-7*((x-3)**3)+2
+// x=`))
+// let y = 4*((x-3)**6)-7*((x-3)**3)+2
+// alert(y)
+
+// let A = Number(prompt
+// (`A=`))
+// let A2 = A**2;
+// let A4 = A**4;
+// let A8 = A**8;
+// alert(`A2=${A2}; A4=${A4}; A8=${A8}`)
+
+let A = Number(prompt
+    (`A=`))
+    let A2 = A**2;
+    let A3 = A**3;
+    let A5 = A**5;
+    let A10 = A**10;
+    let A15 = A**15;
+    alert(`A2=${A2}; A3=${A3}; A5=${A5}; A10=${A10}; A15=${A15}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
